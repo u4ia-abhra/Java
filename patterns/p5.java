1a05b342552660016bd4d583547d4facbb6940d2
Abhrajit Ghosh <157137147+u4ia-abhra@users.noreply.github.com>
2024-06-26 09:35:39 +0530

    Add files via upload

---
+++ patterns/p5.java
@@ -0,0 +1,24 @@
+/* 
+1
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
+ */
+import java.util.Scanner;
+
+public class p5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(((i+j+1)%2)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
